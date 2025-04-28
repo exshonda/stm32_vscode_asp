25ed7b8a126632e43a901d7249a3505d456b0245
長島 宏明 <nagasima@core-s.co.jp>
2025-04-28 15:45:53 +0900

    ビルド設定を整理

--- asp3/asp3_stm32cubemx.cmake
+++ asp3/asp3_stm32cubemx.cmake
@@ -1,10 +1,3 @@
 set(ASP3_ROOT_DIR ${CMAKE_CURRENT_LIST_DIR})
 
 set(ASP3_TARGET stm32cubemx)
-
-#list(APPEND ASP3_COMPILE_DEFS
-#    TOPPERS_OMIT_VECTOR_TABLE
-#)
-
-function(asp3_set_stm32cubemx_options TARGET)
-endfunction()
